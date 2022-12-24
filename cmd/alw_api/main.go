2cd88b1d73ae1c2b29e333a8c16637bb5c24288a
tslight <tslight@pm.com>
2022-12-24 14:16:49 +0000

    more betterer logging ;-)

--- cmd/alw_api/main.go
+++ cmd/alw_api/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/tslight/naeq/assets/books"
 	"github.com/tslight/naeq/pkg/alw"
 	"github.com/tslight/naeq/pkg/efs"
 	j "github.com/tslight/naeq/pkg/json"
+	"io"
 	"log"
 	"net/http"
 )
@@ -23,12 +25,22 @@ type Query struct {
 }
 
 func logRequest(r *http.Request) {
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	// Replace the body with a new reader after reading from the original
+	r.Body = io.NopCloser(bytes.NewBuffer(b))
 	ip := r.RemoteAddr
 	// ip := r.Header.Get("Client-Ip")
 	agent := r.Header.Get("User-Agent")
 	log.Printf(
 		"%s to %s from %s at %s\n", r.Method, r.URL.Path, agent, ip,
 	)
+	bstr := string(b)
+	if bstr != "" {
+		log.Println(bstr)
+	}
 }
 
 func DecodeRequest(r *http.Request, i interface{}) *Error {
@@ -47,6 +59,7 @@ func DecodeRequest(r *http.Request, i interface{}) *Error {
 }
 
 func main() {
+	log.Printf("Synchronicity engines started... Awaiting requests...")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		logRequest(r)
 		if r.URL.Path != "/" {
@@ -73,16 +86,19 @@ func main() {
 				json.NewEncoder(w).Encode(err)
 				break
 			}
+			log.Printf("NAEQ Sum: %d", i)
 			book, err := j.FromEFSPath(books.EFS, query.Book)
 			if err != nil {
 				log.Println(err)
 				json.NewEncoder(w).Encode(err)
 				break
 			}
-			log.Printf("%#v", query)
+			log.Printf("%s = %s", query.Book, book["name"])
+			// log.Printf("%#v", query)
 			matches := alw.GetMatches(i, book)
-			log.Printf("%#v", matches)
+			// log.Printf("%#v", matches)
 			json.NewEncoder(w).Encode(matches)
+			log.Printf("Successfully returned %d matches! :-)", len(matches))
 		case http.MethodPut:
 			// Update an existing record.
 		case http.MethodDelete:
