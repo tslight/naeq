029338d19dfb3fa7730d010f46b3ff368e689d31
tslight <tslight@pm.com>
2022-12-28 12:46:12 +0000

    make test more robust

--- cmd/alw_api/main_test.go
+++ cmd/alw_api/main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -17,12 +20,43 @@ func TestGetRequest(t *testing.T) {
 }
 
 func TestValidPostRequest(t *testing.T) {
-	json := strings.NewReader("{\"book\": \"liber-al.json\", \"words\": \"foo\"}")
-	req := httptest.NewRequest(http.MethodPost, "/", json)
+	data := strings.NewReader("{\"book\": \"liber-al.json\", \"words\": \"foo\"}")
+	req := httptest.NewRequest(http.MethodPost, "/", data)
 	w := httptest.NewRecorder()
 	Handler(w, req)
 	if want, got := http.StatusOK, w.Result().StatusCode; want != got {
 		t.Fatalf("expected a %d, instead got: %d", want, got)
+	}
+	expectedBody := Response{
+		Book:       "The Book of the Law",
+		Sum:        32,
+		MatchCount: 15,
+		Matches: append(make([]interface{}, 0),
+			"3 8 a b",
+			"a b k 2",
+			"door",
+			"g m",
+			"go who",
+			"his",
+			"kaaba",
+			"laid",
+			"last",
+			"lords",
+			"loud",
+			"oil",
+			"shall call",
+			"well",
+			"what",
+		),
+	}
+	b, err := io.ReadAll(w.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	receivedBody := Response{}
+	json.Unmarshal(b, &receivedBody)
+	if fmt.Sprintf("%#v", expectedBody) != fmt.Sprintf("%#v", receivedBody) {
+		t.Fatalf("expected a %#v, instead got: %#v", expectedBody, receivedBody)
 	}
 }
 
