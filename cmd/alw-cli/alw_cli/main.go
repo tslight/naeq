d9d5da05b4d052c476f33feabf1702694b750664
tslight <tslight@pm.com>
2022-12-30 20:47:59 +0000

    simplify command line entrypoint

--- cmd/alw-cli/alw_cli/main.go
+++ cmd/alw-cli/alw_cli/main.go
@@ -12,22 +12,25 @@ import (
 	"os"
 )
 
+var (
+	count   = flag.Int("n", 0, "number of matches to show")
+	path    = flag.String("p", "", "path to alternative book")
+	efsBook = flag.String("b", "liber-al", "embedded book")
+	list    = flag.Bool("l", false, "list embedded books")
+	sum     = flag.Bool("s", false, "only return naeq sum")
+	version = flag.Bool("v", false, "print version info")
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage: %s [options...] <words>:\n", os.Args[0])
+	flag.PrintDefaults()
+}
+
 func main() {
-	var count int
-	var path, efsBook string
-	var list, sum bool
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s [options...] <words>:\n", os.Args[0])
-		flag.PrintDefaults()
-	}
-	flag.IntVar(&count, "n", 0, "number of matches to show")
-	flag.StringVar(&path, "p", "", "path to alternative book")
-	flag.StringVar(&efsBook, "b", "liber-al", "embedded book")
-	flag.BoolVar(&list, "l", false, "list embedded books")
-	flag.BoolVar(&sum, "s", false, "only return naeq sum")
+	flag.Usage = usage
 	flag.Parse()
 
-	if list {
+	if *list {
 		bookNames, err := efs.GetBaseNamesSansExt(&books.EFS)
 		if err != nil {
 			log.Fatalln(err)
@@ -48,19 +51,19 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	if sum {
+	if *sum {
 		fmt.Println(i)
 		return
 	}
 
 	var book map[string]interface{}
-	if path != "" {
-		book, err = json.FromPath(path)
+	if *path != "" {
+		book, err = json.FromPath(*path)
 		if err != nil {
 			log.Fatalln(err)
 		}
 	} else {
-		book, err = json.FromEFSPath(books.EFS, fmt.Sprint(efsBook, ".json"))
+		book, err = json.FromEFSPath(books.EFS, fmt.Sprint(*efsBook, ".json"))
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -69,7 +72,7 @@ func main() {
 	matches := alw.GetMatches(i, book)
 
 	for k, v := range matches {
-		if count > 0 && k >= count {
+		if *count > 0 && k >= *count {
 			break
 		}
 		fmt.Println(v)
