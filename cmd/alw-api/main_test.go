72d3d0ebf23671e6a1a273b4b78dcbac6601f470
tslight <tslight@pm.com>
2022-12-31 12:06:37 +0000

    add query param tests

--- cmd/alw-api/main_test.go
+++ cmd/alw-api/main_test.go
@@ -19,9 +19,8 @@ func TestGetRequest(t *testing.T) {
 	}
 }
 
-func TestValidPostRequest(t *testing.T) {
-	data := strings.NewReader("{\"book\": \"liber-al.json\", \"words\": \"foo\"}")
-	req := httptest.NewRequest(http.MethodPost, "/", data)
+func TestValidGetQueryParamsWithNoBook(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/?words=foo", nil)
 	w := httptest.NewRecorder()
 	Handler(w, req)
 	if want, got := http.StatusOK, w.Result().StatusCode; want != got {
@@ -46,6 +45,38 @@ func TestValidPostRequest(t *testing.T) {
 			"oil",
 			"shall call",
 			"well",
+			"what",
+		),
+	}
+	b, err := io.ReadAll(w.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	receivedBody := Response{}
+	err = json.Unmarshal(b, &receivedBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if fmt.Sprintf("%#v", expectedBody) != fmt.Sprintf("%#v", receivedBody) {
+		t.Fatalf("expected a %#v, instead got: %#v", expectedBody, receivedBody)
+	}
+}
+
+func TestValidGetQueryParamsWithBook(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/?words=foo&book=liber-i.json", nil)
+	w := httptest.NewRecorder()
+	Handler(w, req)
+	if want, got := http.StatusOK, w.Result().StatusCode; want != got {
+		t.Fatalf("expected a %d, instead got: %d", want, got)
+	}
+	expectedBody := Response{
+		Book:       "The Book of the Magus",
+		Sum:        32,
+		MatchCount: 4,
+		Matches: append(make([]interface{}, 0),
+			"his",
+			"last",
+			"{{11}} his",
 			"what",
 		),
 	}
