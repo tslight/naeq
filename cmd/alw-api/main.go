2235ef440ca3554d16bb1a29b0d0b44f9696ba3f
tslight <tslight@pm.com>
2023-01-01 20:31:41 +0000

    implement basic custom logger with loglevels

--- cmd/alw-api/main.go
+++ cmd/alw-api/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
-	"log"
 	"net/http"
 	"os"
 
@@ -14,6 +11,7 @@ import (
 	"github.com/tslight/naeq/pkg/alw"
 	"github.com/tslight/naeq/pkg/efs"
 	j "github.com/tslight/naeq/pkg/json"
+	"github.com/tslight/naeq/pkg/log"
 )
 
 var (
@@ -36,28 +34,6 @@ type Response struct {
 	Sum        int           `json:"sum"`
 	MatchCount int           `json:"match_count"`
 	Matches    []interface{} `json:"matches"`
-}
-
-func logRequest(r *http.Request) {
-	b, err := io.ReadAll(r.Body)
-	if err != nil {
-		log.Println(err)
-	}
-	// Replace the body with a new reader after reading from the original
-	r.Body = io.NopCloser(bytes.NewBuffer(b))
-	ip := r.RemoteAddr
-	// ip := r.Header.Get("Client-Ip")
-	agent := r.Header.Get("User-Agent")
-	log.Printf(
-		"%s to %s from %s at %s\n", r.Method, r.URL.Path, agent, ip,
-	)
-	if r.URL.RawQuery != "" {
-		log.Printf("Query Params: %s", r.URL.RawQuery)
-	}
-	bstr := string(b)
-	if bstr != "" {
-		log.Println(bstr)
-	}
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) error {
@@ -102,14 +78,14 @@ func buildResponse(words string, book string) (interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
-	log.Printf("NAEQ Sum: %d", i)
+	log.Info.Printf("NAEQ Sum: %d", i)
 	b, err := j.FromEFSPath(books.EFS, fmt.Sprint(book, ".json"))
 	if err != nil {
 		return nil, err
 	}
-	log.Printf("%s = %s (%s)", book, b["liber"], b["name"])
+	log.Info.Printf("%s = %s (%s)", book, b["liber"], b["name"])
 	matches := alw.GetMatches(i, b)
-	log.Printf("Successfully found %d matches! :-)", len(matches))
+	log.Info.Printf("Successfully found %d matches! :-)", len(matches))
 	response := Response{
 		Liber:      b["liber"],
 		Book:       b["name"],
@@ -125,7 +101,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	var err error
 	var book string
 
-	logRequest(r)
+	log.Request(r)
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -140,14 +116,14 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			}
 			response, err = buildResponse(words, book)
 			if err != nil {
-				log.Println(err)
+				log.Error.Println(err)
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
 		} else {
 			err := aboutHandler(w, r)
 			if err != nil {
-				log.Println(err)
+				log.Error.Println(err)
 			}
 			return
 		}
@@ -157,13 +133,13 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
 		decoder.DisallowUnknownFields()
 		if err := decoder.Decode(&data); err != nil {
-			log.Println(err)
+			log.Error.Println(err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 		response, err = buildResponse(data.Words, data.Book)
 		if err != nil {
-			log.Println(err)
+			log.Error.Println(err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -173,7 +149,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 	err = json.NewEncoder(w).Encode(response)
 	if err != nil {
-		log.Println(err)
+		log.Error.Println(err)
 	}
 }
 
@@ -183,12 +159,12 @@ func main() {
 		fmt.Println(Version)
 		return
 	}
-	log.Println("Synchronicity engines starting...")
+	log.Info.Println("Synchronicity engines starting...")
 	http.HandleFunc("/", Handler)
 	envPort, envPortPresent := os.LookupEnv("PORT")
 	if envPortPresent {
-		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", envPort), nil))
+		log.Error.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", envPort), nil))
 	} else {
-		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+		log.Error.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 	}
 }
---
+++ pkg/log/log.go
@@ -0,0 +1,96 @@
+package log
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	Debug *log.Logger
+	Info  *log.Logger
+	Warn  *log.Logger
+	Error *log.Logger
+)
+
+func setLogLevel(outMW io.Writer) {
+	isDebug, isDebugPresent := os.LookupEnv("DEBUG")
+	if isDebugPresent && strings.ToLower(isDebug) == "true" {
+		Debug.SetOutput(outMW)
+		Debug.Println("LOGLEVEL set to [DEBUG]")
+		return
+	}
+	level, levelPresent := os.LookupEnv("LOGLEVEL")
+	if levelPresent {
+		switch strings.ToLower(level) {
+		case "1", "debug", "trace":
+			Debug.SetOutput(outMW)
+			Debug.Println("LOGLEVEL set to " + level)
+		case "2", "info", "information":
+			Debug.SetOutput(io.Discard)
+		case "3", "warn", "warning":
+			Debug.SetOutput(io.Discard)
+			Info.SetOutput(io.Discard)
+		case "4", "err", "error":
+			Debug.SetOutput(io.Discard)
+			Info.SetOutput(io.Discard)
+			Warn.SetOutput(io.Discard)
+		default:
+			Warn.Printf("LOGLEVEL=%s is INVALID! Using default [INFO].", level)
+		}
+	}
+}
+
+func init() {
+	var file *os.File
+	var err error
+
+	envFile, envFilePresent := os.LookupEnv("LOGFILE")
+	if envFilePresent {
+		log.Println("Logging to " + envFile)
+		file, err = os.OpenFile(envFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	} else {
+		t := time.Now().Format("20060102150405")
+		file, err = os.OpenFile(t+".txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	outMW := io.MultiWriter(os.Stdout, file)
+	errMW := io.MultiWriter(os.Stderr, file)
+
+	Debug = log.New(io.Discard, "[DEBUG] ", log.Ldate|log.Ltime|log.Lshortfile)
+	Info = log.New(outMW, "[INFO] ", log.LstdFlags)
+	Warn = log.New(outMW, "[WARNING] ", log.LstdFlags)
+	Error = log.New(errMW, "[ERROR] ", log.LstdFlags)
+
+	setLogLevel(outMW)
+}
+
+func Request(r *http.Request) {
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		Error.Fatal(err)
+	}
+	// Replace the body with a new reader after reading from the original
+	r.Body = io.NopCloser(bytes.NewBuffer(b))
+	ip := r.RemoteAddr
+	// ip := r.Header.Get("Client-Ip")
+	agent := r.Header.Get("User-Agent")
+	Info.Printf(
+		"%s to %s from %s at %s\n", r.Method, r.URL.Path, agent, ip,
+	)
+	if r.URL.RawQuery != "" {
+		Info.Printf("Query Params: %s", r.URL.RawQuery)
+	}
+	bstr := string(b)
+	if bstr != "" {
+		Info.Println(bstr)
+	}
+}
