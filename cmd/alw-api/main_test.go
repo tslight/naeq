e518b521b2b57246cb3f0b3fe0b38e0a33650dce
tslight <tslight@pm.com>
2023-01-02 23:28:38 +0000

    add back post tests

--- cmd/alw-api/main_test.go
+++ cmd/alw-api/main_test.go
@@ -96,6 +96,85 @@ func TestValidGetQueryParamsWithBook(t *testing.T) {
 	}
 }
 
+func TestValidPostRequestWithoutBook(t *testing.T) {
+	data := strings.NewReader("{\"words\": \"foo\"}")
+	req := httptest.NewRequest(http.MethodPost, "/", data)
+	w := httptest.NewRecorder()
+	handler(w, req)
+	if want, got := http.StatusOK, w.Result().StatusCode; want != got {
+		t.Fatalf("expected a %d, instead got: %d", want, got)
+	}
+	expectedBody := Response{
+		Liber:      "Liber XXXI",
+		Book:       "The Book of the Law",
+		Sum:        32,
+		MatchCount: 15,
+		Matches: append(make([]interface{}, 0),
+			"3 8 a b",
+			"a b k 2",
+			"door",
+			"g m",
+			"go who",
+			"his",
+			"kaaba",
+			"laid",
+			"last",
+			"lords",
+			"loud",
+			"oil",
+			"shall call",
+			"well",
+			"what",
+		),
+	}
+	b, err := io.ReadAll(w.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	receivedBody := Response{}
+	err = json.Unmarshal(b, &receivedBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if fmt.Sprintf("%#v", expectedBody) != fmt.Sprintf("%#v", receivedBody) {
+		t.Fatalf("expected a %#v, instead got: %#v", expectedBody, receivedBody)
+	}
+}
+
+func TestValidPostRequestWithBook(t *testing.T) {
+	data := strings.NewReader("{\"book\": \"liber-i\", \"words\": \"foo\"}")
+	req := httptest.NewRequest(http.MethodPost, "/", data)
+	w := httptest.NewRecorder()
+	handler(w, req)
+	if want, got := http.StatusOK, w.Result().StatusCode; want != got {
+		t.Fatalf("expected a %d, instead got: %d", want, got)
+	}
+	expectedBody := Response{
+		Liber:      "Liber I",
+		Book:       "The Book of the Magus",
+		Sum:        32,
+		MatchCount: 4,
+		Matches: append(make([]interface{}, 0),
+			"his",
+			"last",
+			"(Verse 11) his",
+			"what",
+		),
+	}
+	b, err := io.ReadAll(w.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	receivedBody := Response{}
+	err = json.Unmarshal(b, &receivedBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if fmt.Sprintf("%#v", expectedBody) != fmt.Sprintf("%#v", receivedBody) {
+		t.Fatalf("expected a %#v, instead got: %#v", expectedBody, receivedBody)
+	}
+}
+
 func TestInvalidFieldPostRequest(t *testing.T) {
 	json := strings.NewReader("{\"book\": \"liber-al\", \"word\": \"foo\"}")
 	req := httptest.NewRequest(http.MethodPost, "/", json)
